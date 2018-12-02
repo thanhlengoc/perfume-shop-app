6538238628d8494413368b5218b50471b5aafbd3
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-12-02 16:08:31 +0700

    SearchView: --Navigator-- switch screen: ProductDetails-SearchView

--- components/Main/Shop/Search/Search.js
+++ components/Main/Shop/Search/Search.js
@@ -1,16 +1,23 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
+import React, { Component } from 'react';
+import { Navigator } from 'react-native-deprecated-custom-components';
 
-export default class Search extends Component {
-    constructor(props){
-        super(props);
-    }
+import SearchView from './SearchView';
+import ProductDetail from '../ProductDetail/ProductDetail';
 
+class Search extends Component {
     render() {
         return (
-            <View>
-                
-            </View>
-        )
+            <Navigator
+                initialRoute={{ name: 'SEARCH_VIEW' }}
+                renderScene={(route, navigator) => {
+                    switch (route.name) {
+                        case 'SEARCH_VIEW': return <SearchView navigator={navigator} />;
+                        default: return <ProductDetail navigator={navigator} product={route.product} />;
+                    }
+                }}
+            />
+        );
     }
 }
+
+export default Search;
