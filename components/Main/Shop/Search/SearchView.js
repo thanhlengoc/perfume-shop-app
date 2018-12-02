ec52aa5ec2b85215d9fd2d08310e8902686bd7fc
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-12-02 16:01:29 +0700

    SearchView: set api + set styles

---
+++ components/Main/Shop/Search/SearchView.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet, Text, TouchableOpacity,
+    ListView, View, Image, Dimensions
+} from 'react-native';
+import global from '../../../global';
+
+const url = 'http://localhost/api/images/product/';
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+}
+
+class SearchView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        const {
+            product, mainRight, txtMaterial, txtColor,
+            txtName, txtPrice, productImage,
+            txtShowDetail, showDetailContainer, wrapper
+        } = styles;
+        return (
+
+        );
+    }
+}
+
+const { width } = Dimensions.get('window');
+const imageWidth = width / 4;
+const imageHeight = (imageWidth * 452) / 361;
+
+const styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#DFDFDF',
+        flex: 1
+    },
+    product: {
+        flexDirection: 'row',
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 2,
+        shadowColor: '#3B5458',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2
+    },
+    productImage: {
+        width: imageWidth,
+        height: imageHeight,
+        flex: 1,
+        resizeMode: 'center'
+    },
+    mainRight: {
+        flex: 3,
+        justifyContent: 'space-between'
+    },
+    productController: {
+        flexDirection: 'row'
+    },
+    numberOfProduct: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    txtName: {
+        paddingLeft: 20,
+        color: '#A7A7A7',
+        fontSize: 20,
+        fontWeight: '400',
+        fontFamily: 'Avenir'
+    },
+    txtPrice: {
+        paddingLeft: 20,
+        color: '#C21C70',
+        fontSize: 15,
+        fontWeight: '400',
+        fontFamily: 'Avenir'
+    },
+    txtColor: {
+        paddingLeft: 20,
+        color: 'black',
+        fontSize: 15,
+        fontWeight: '400',
+        fontFamily: 'Avenir'
+    },
+    txtMaterial: {
+        paddingLeft: 20,
+        color: 'black',
+        fontSize: 15,
+        fontWeight: '400',
+        fontFamily: 'Avenir'
+    },
+    txtShowDetail: {
+        color: '#C21C70',
+        fontSize: 10,
+        fontWeight: '400',
+        fontFamily: 'Avenir',
+        textAlign: 'right',
+    },
+    showDetailContainer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        alignSelf: 'flex-end',
+        marginTop: 100
+    }
+});
+
+export default SearchView;
